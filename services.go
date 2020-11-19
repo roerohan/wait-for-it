8d5812ee118aca1c7f65be5a9a687fc76fbab975
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-19 18:22:55 +0530

    feat: Added logic for waiting

--- services.go
+++ services.go
@@ -27,3 +27,41 @@ func (s *Services) Set(value string) error {
 func (s *Services) String() string {
 	return strings.Join(*s, ", ")
 }
+
+// Wait waits for all services
+func (s *Services) Wait(tSeconds int) bool {
+	t := time.Duration(tSeconds) * time.Second
+	now := time.Now()
+
+	var wg sync.WaitGroup
+	wg.Add(len(*s))
+
+	success := make(chan bool, 1)
+
+	go func() {
+		for _, service := range services {
+			go waitOne(service, &wg, now)
+		}
+		wg.Wait()
+		success <- true
+	}()
+
+	select {
+	case <-success:
+		return true
+	case <-time.After(t):
+		return false
+	}
+}
+
+func waitOne(service string, wg *sync.WaitGroup, start time.Time) {
+	defer wg.Done()
+	for {
+		_, err := net.Dial("tcp", service)
+		if err == nil {
+			Log(fmt.Sprintf("%s is available after %s", service, time.Since(start)))
+			break
+		}
+		time.Sleep(time.Second)
+	}
+}
