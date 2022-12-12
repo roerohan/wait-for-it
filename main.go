7d5bff25b58e6a8bd960eb3bcd9ae926a4fceacb
Samantha Coyle <samantha.coyle@intel.com>
2022-12-12 14:15:28 -0600

    refactor(service): use service struct with appropriate fields
    Signed-off-by: Samantha Coyle <samantha.coyle@intel.com>

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/roerohan/wait-for-it/pkg/wait"
 	"os"
+	"time"
 )
 
 var (
@@ -15,10 +16,10 @@ var (
 )
 
 func init() {
-	flag.IntVar(&timeout, "t", 15, "Timeout in seconds, zero for no timeout")
+	flag.IntVar(&timeout, "t", 15, "Service request timeout in seconds, zero for no timeout")
 	flag.BoolVar(&quiet, "q", false, "Quiet, don't output any status messages")
 	flag.BoolVar(&strict, "s", false, "Only execute subcommand if the test succeeds")
-	flag.Var(&services, "w", "Services to be waiting for, in the form `host:port`")
+	flag.Var(&services, "w", "Dependency services to be waiting for, in the form `host:port`")
 }
 
 // Log is used to log with prefix wait-for-it:
@@ -36,10 +37,10 @@ func main() {
 
 	if len(services) != 0 {
 		log(fmt.Sprintf("waiting %d seconds for %s", timeout, services.String()))
-		ok := services.Wait(timeout)
-
-		if !ok {
+		err := wait.ForDependencies(services, time.Duration(timeout), 30*time.Second)
+		if err != nil {
 			log(fmt.Sprintf("timeout occured after waiting for %d seconds", timeout))
+			log(fmt.Sprintf("wait.ForDependencies failed with err %v", err))
 			if strict {
 				log("strict mode, refusing to execute subprocess")
 				return
--- pkg/wait/wait.go
+++ pkg/wait/wait.go
@@ -1,37 +1,62 @@
 package wait
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
-// Services is a string array storing
-// the services that are to be waited for
+// Services is a string array storing the services that are to be waited for
 type Services []Service
 
-// Service is a string meant to denote a service with a wait condition upon start up
-type Service string
-
-func (s *Service) String() string {
-	return string(*s)
+// Service is a struct meant to denote a service hostname:port with a wait condition upon start up
+type Service struct {
+	hostname string
+	port     int
 }
 
-// Set is used to append a Service
-// to the slice of Services, to implement
-// the interface flag.Value
+var (
+	// ErrServiceMaxTimeout is the error message to use in case a max service startup timeout is exceeded.
+	ErrServiceMaxTimeout = fmt.Errorf("max service startup timeout duration exceeded waiting for service dependencies")
+)
+
+// String prints out the human-readable Service hostname:port string.
+func (s *Service) String() string {
+	return fmt.Sprintf("%s:%d", s.hostname, s.port)
+}
+
+// NewService creates a wait.Service type.
+func NewService(hostname string, port int) Service {
+	return Service{
+		hostname: hostname,
+		port:     port,
+	}
+}
+
+// Set is used to append a Service to the slice of Services,
+// to implement the interface flag.Value
 func (s *Services) Set(value string) error {
-	service := interface{}(value).(Service)
+	const separator = ":"
+
+	// Note: serviceInfo[0] = hostname, serviceInfo[1] = port
+	serviceInfo := strings.Split(value, separator)
+	Log(fmt.Sprintf("serviceInfo %s", serviceInfo))
+	port, err := strconv.Atoi(serviceInfo[1])
+	if err != nil {
+		return err
+	}
+
+	service := NewService(serviceInfo[0], port)
 	*s = append(*s, service)
 	return nil
 }
 
-// String returns a string
-// representation of the flag,
-// to implement the interface
-// flag.Value
+// String returns a string representation of the flag,
+// to implement the interface flag.Value
 func (s *Services) String() string {
 	var sb strings.Builder
 	const formatter string = ", "
@@ -42,18 +67,65 @@ func (s *Services) String() string {
 	return sb.String()
 }
 
-// Wait waits for all services
-func (s *Services) Wait(tSeconds int) bool {
-	t := time.Duration(tSeconds) * time.Second
+//func ForDependencies(waitServices Services, serviceRequestTimeout time.Duration) error {
+//	//serviceTimeout := int(serviceRequestTimeout.Seconds())
+//	success := make(chan bool, 1)
+//
+//	if len(waitServices) != 0 {
+//		//lc.Infof("Service startup timeout invoked to wait %d seconds for dependent services %s", serviceTimeout, dependentServices)
+//		ok := wait(waitServices, serviceRequestTimeout)
+//		if ok {
+//			success <- true
+//		} else {
+//			Log("Waiting for service dependencies to become available...")
+//		}
+//	}
+//
+//	// return err if service wait time exceeds ServiceMaxTimeout time
+//	select {
+//	case <-success:
+//		return nil
+//	//case <-time.After(ServiceMaxTimeout):
+//	//	return ErrServiceMaxTimeout
+//	}
+//	return nil
+//}
+
+// ForDependencies allows the service to wait for its dependencies to be up and ready for a configurable amount of time.
+// If the service dependency request timeout is reached and the dependent services are not yet available,
+// then the timeout wait interval will continue until the dependencies are up for a maximum wait time of maxTimeout.
+func ForDependencies(waitServices Services, serviceRequestTimeout, maxTimeout time.Duration) error {
+	success := make(chan bool, 1)
+
+	if len(waitServices) != 0 {
+		//lc.Infof("Service startup timeout invoked to wait %d seconds for dependent services %s", serviceTimeout, dependentServices)
+		ok := wait(waitServices, serviceRequestTimeout)
+		if ok {
+			success <- true
+		} else {
+			Log("Waiting for service dependencies to become available...")
+		}
+	}
+
+	// return err if service wait time exceeds ServiceMaxTimeout time
+	select {
+	case <-success:
+		return nil
+	case <-time.After(maxTimeout):
+		return ErrServiceMaxTimeout
+	}
+}
+
+func wait(waitServices Services, waitTimeOut time.Duration) bool {
 	now := time.Now()
 
 	var wg sync.WaitGroup
-	wg.Add(len(*s))
+	wg.Add(len(waitServices))
 
 	success := make(chan bool, 1)
 
 	go func() {
-		for _, service := range *s {
+		for _, service := range waitServices {
 			go waitOne(service, &wg, now)
 		}
 		wg.Wait()
@@ -63,7 +135,7 @@ func (s *Services) Wait(tSeconds int) bool {
 	select {
 	case <-success:
 		return true
-	case <-time.After(t):
+	case <-time.After(waitTimeOut):
 		return false
 	}
 
@@ -74,9 +146,14 @@ func waitOne(service Service, wg *sync.WaitGroup, start time.Time) {
 	for {
 		_, err := net.Dial("tcp", service.String())
 		if err == nil {
-			Log(fmt.Sprintf("%s is available after %s", service, time.Since(start)))
+			Log(fmt.Sprintf("%s is available after %s", service.String(), time.Since(start)))
+			break
 		}
-		break
+		opErr, ok := err.(*net.OpError)
+		if ok && errors.Is(err, opErr) {
+			Log(fmt.Sprintf("failed to dial service %s with error: %s", service.String(), opErr.Error()))
+			break
+		}
+		time.Sleep(time.Second)
 	}
-	time.Sleep(time.Second)
 }
